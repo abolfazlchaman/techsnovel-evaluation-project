666f11bc2f6dbf57afb4c1b3638ec46fa944cffa
Abolfazl Chaman <88617114+abolfazlchaman@users.noreply.github.com>
2024-05-23 17:50:34 +0330

    added the missing useState import

--- app/components/UsersList/UsersList.tsx
+++ app/components/UsersList/UsersList.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
     fetchUsers,
